8818d31c00e1c2e755247d31c06cd3304455adb3
andresdev <andresmestra11a@gmail.com>
2021-02-12 19:47:40 -0500

    Buscar mensajes en db

--- controllers/mensajes.js
+++ controllers/mensajes.js
@@ -1,14 +1,35 @@
-const { response } = require("express");
+const { response, request } = require("express");
+const Mensaje = require("../models/mensaje");
 
 
-const obtenerChat = ( req, res = response ) => {
-  
+const obtenerChat = async (req = request, res = response) => {
+
   const miId = req.uid;
+  const mensajeDe = req.params.de;
 
-  return res.json({
-    ok: true,
-    miId,
-  })
+  try {
+
+    const last30 = await Mensaje.find({
+      $or: [
+        { de: miId, para: mensajeDe },
+        { de: mensajeDe, para: miId },
+      ]
+    })
+      .sort({ createdAt: 'desc' })
+      .limit(30)
+
+    return res.json({
+      ok: true,
+      mensajes: last30,
+    })
+
+  } catch (error) {
+    console.log(error)
+    return res.status(500).json({
+      ok: false,
+      msg: 'Hable con el administrador'
+    })
+  }
 
 }
 
